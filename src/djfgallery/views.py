91f742468f2cd71024a733e33bc09e202a6cf1ce
Tony G. Bolaño <tonybolanyo@gmail.com>
2018-01-23 15:09:12 +0100

    Playing with flickrapi

--- src/djfgallery/views.py
+++ src/djfgallery/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+import json
+import flickrapi
 
-# Create your views here.
+from django.views.generic import TemplateView
+
+
+class AlbumListView(TemplateView):
+    template_name = "gallery/album_list.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        api_key = u'6cf69c2dd5bcc4034f999f7d53e498cc'
+        api_secret = u'd8c0b93b0a1305b4'
+
+        flickr = flickrapi.FlickrAPI(api_key, api_secret, format='json')
+        # photos = flickr.photos.search(user_id='73509078@N00', per_page='10')
+        sets = flickr.photosets.getList(user_id='76341745@N00')
+        context['album_list'] = json.loads(sets.decode('utf-8'))
+        return context
--- src/djflickr/urls.py
+++ src/djflickr/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from djfgallery.views import AlbumListView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('albums/', AlbumListView.as_view())
 ]
